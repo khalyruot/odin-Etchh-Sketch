5f480495cbc1cb1045f85a7ea65af07de268304a
khaly <khalyruot12345@gmail.com>
2023-09-07 13:29:39 -0700

    try to create div  using Javascript

---
+++ script.js
@@ -0,0 +1,10 @@
+
+const container = document.querySelector('#container');
+const content = document.createElement('div');
+
+content.classList.add('content');
+content.textContent = "A";
+
+container.appendChild(content);
+
+ 
