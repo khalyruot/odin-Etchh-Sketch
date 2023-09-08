5f0ffcb321827b81ad9ec792248ad94889f539bd
khaly <khalyruot12345@gmail.com>
2023-09-08 11:59:03 -0700

    add for loop in javascript to create 16 divs

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
 
 const container = document.querySelector('#container');
-const content = document.createElement('div');
 
-content.classList.add('content');
-content.textContent = "A";
 
-container.appendChild(content);
+for(i = 0; i < 16; i++){
+    
+    var content = document.createElement('div');
+    content.classList.add('content');
+    content.textContent = i;
+    container.appendChild(content);
+
+}
+
+
 
  
