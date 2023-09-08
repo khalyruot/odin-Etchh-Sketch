8fe0f25dc864b716d70eecb5db3f91e76c0a7c70
khaly <khalyruot12345@gmail.com>
2023-09-08 13:41:17 -0700

    create 16 x 16 grid on javascript

--- script.js
+++ script.js
@@ -1,15 +1,28 @@
 
-const container = document.querySelector('#container');
+var container = document.querySelector('#container');
 
 
 for(i = 0; i < 16; i++){
-    
+
     var content = document.createElement('div');
     content.classList.add('content');
-    content.textContent = i;
+    content.textContent = "a";
     container.appendChild(content);
 
+    for(j = 0; j < 16; j++){
+        var sub_content = document.createElement('div');
+        sub_content.classList.add('sub_content');
+        sub_content.textContent = "a";
+        content.appendChild(sub_content);
+    
+    }
+
+    
 }
+
+
+
+
 
 
 
