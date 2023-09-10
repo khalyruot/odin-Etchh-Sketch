9d0d4437547114e4890a6fbab51e818e5f03bf6e
khaly <khalyruot12345@gmail.com>
2023-09-09 20:42:27 -0700

    add javascript  logic

--- script.js
+++ script.js
@@ -2,22 +2,44 @@
 var container = document.querySelector('#container');
 
 
-for(i = 0; i < 16; i++){
+for(let i = 0; i < 16; i++){
 
     var content = document.createElement('div');
     content.classList.add('content');
     container.appendChild(content);
 
-    for(j = 0; j < 16; j++){
+    for(let j = 0; j < 16; j++){
         var sub_content = document.createElement('div');
+        
+       
+
         sub_content.classList.add('sub_content');
-        sub_content.textContent = "a";
+        sub_content.textContent = " ";
         content.appendChild(sub_content);
-    
+
+        
+        
     }
 
+    const box_hover = document.querySelectorAll('.sub_content');
+
+    for(let k = 0; k < box_hover.length; k++){
+        
+        box_hover[k].onmouseenter = function(){mouseEnter()};
+        function mouseEnter(){
+            box_hover[k].style.backgroundColor ="red";
+        }
+    }
+
+
+   
+        
     
 }
+
+
+
+
 
 
 
