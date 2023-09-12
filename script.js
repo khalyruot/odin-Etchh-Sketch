bbdd8333f0f983d2ad954d0ac443d3cc6420d929
khaly <khalyruot12345@gmail.com>
2023-09-12 14:02:55 -0700

    testing the for loop in order to delete previous iteration

--- script.js
+++ script.js
@@ -1,43 +1,63 @@
 
 var container = document.querySelector('#container');
 
-const slider = document.getElementById("myRange");
-const output = document.getElementById("demo");
+
+var slider = document.getElementById("myRange");
+var output = document.getElementById("demo");
+var number = slider.value;
 output.innerHTML = slider.value;
 
 
 slider.oninput = function() {
   output.innerHTML = this.value;
-  console.log("output" + output.innerHTML);
-  for(let i = 0; i < output.innerHTML; i++){
+  number = output.innerHTML;
+  console.log("arrray_number: "  + number);
 
-    var content = document.createElement('div');
-    content.classList.add('content');
-    container.appendChild(content);
 
-    for(let j = 0; j < output.innerHTML; j++){
-        var sub_content = document.createElement('div');
+        for(let i = 0; i < output.innerHTML; i++){
 
-        sub_content.classList.add('sub_content');
-        sub_content.textContent = " ";
-        content.appendChild(sub_content);
+            if(output.innerHTML !== 10){
+                var content = document.createElement('div');
+                content.classList.add('content');
+                container.appendChild(content);
         
-    }
+                for(let j = 0; j < output.innerHTML; j++){
+                    var sub_content = document.createElement('div');
+        
+                    sub_content.classList.add('sub_content');
+                    sub_content.textContent = " ";
+                    content.appendChild(sub_content);
+                    
+                }
+        
+                const box_hover = document.querySelectorAll('.sub_content');
+        
+                for(let k = 0; k < box_hover.length; k++){
+        
+                    box_hover[k].onmouseenter = function(){mouseEnter()};
+                    function mouseEnter(){
+                        box_hover[k].style.backgroundColor ="red";
+                    }
+                } 
+            }
+            else{
+                i=-1;
+            }
 
-    const box_hover = document.querySelectorAll('.sub_content');
+            
 
-    for(let k = 0; k < box_hover.length; k++){
-
-        box_hover[k].onmouseenter = function(){mouseEnter()};
-        function mouseEnter(){
-            box_hover[k].style.backgroundColor ="red";
+    
+            
+            
         }
-    }   
+    
     
 }
 
+    
 
-}
+
+
 
 
 
