79be591f9ffbf661ddd46deecef4803f0d9f5bf2
khaly <khalyruot12345@gmail.com>
2023-09-17 18:48:19 -0700

    90% done

--- script.js
+++ script.js
@@ -1,20 +1,18 @@
-var container = document.querySelector('#container')
+var container = document.querySelector('.grid_area');
 
 function creatGrid4x4(){
     
     for(let i = 0; i < 4; i++){
 
-        var content = document.createElement('div');
-        content.classList.add('content');
-        container.appendChild(content);
-    
+        var content4x4_row = document.createElement('div');
+        content4x4_row.classList.add('content');
+        container.appendChild(content4x4_row);
     
         for(let j = 0; j < 4; j++){
-            var sub_content = document.createElement('div');
-    
-            sub_content.classList.add('sub_content');
-            sub_content.textContent = " ";
-            content.appendChild(sub_content);
+            var content4x4_column = document.createElement('div');
+            content4x4_column.textContent = " ";
+            content4x4_row.appendChild(content4x4_column);
+            content4x4_column.classList.add('sub_content');
             
         }
     
@@ -37,17 +35,16 @@ function creatGrid8x8(){
     
     for(let i = 0; i < 8; i++){
 
-        var content = document.createElement('div');
-        content.classList.add('content');
-        container.appendChild(content);
+        var content8x8_row = document.createElement('div');
+        content8x8_row.classList.add('content');
+        container.appendChild(content8x8_row);
     
     
         for(let j = 0; j < 8; j++){
-            var sub_content = document.createElement('div');
-    
-            sub_content.classList.add('sub_content');
-            sub_content.textContent = " ";
-            content.appendChild(sub_content);
+            var content8x8_column = document.createElement('div');
+            content8x8_column.textContent = " ";
+            content8x8_row.appendChild(content8x8_column);
+            content8x8_column.classList.add('sub_content');
             
         }
     
@@ -70,17 +67,16 @@ function creatGrid16x16(){
     
     for(let i = 0; i < 16; i++){
 
-        var content = document.createElement('div');
-        content.classList.add('content');
-        container.appendChild(content);
+        var content16x16_row = document.createElement('div');
+        content16x16_row.classList.add('content');
+        container.appendChild(content16x16_row);
     
     
         for(let j = 0; j < 16; j++){
-            var sub_content = document.createElement('div');
-    
-            sub_content.classList.add('sub_content');
-            sub_content.textContent = " ";
-            content.appendChild(sub_content);
+            var content16x16_column = document.createElement('div');
+            content16x16_column.textContent = " ";
+            content16x16_row.appendChild(content16x16_column);
+            content16x16_column.classList.add('sub_content');
             
         }
     
@@ -99,27 +95,26 @@ function creatGrid16x16(){
     
 }
 
-const fourBYfour = document.getElementById("fourBYfour");
-const eightBYeight = document.getElementById("eightBYeight");
-const sixteenBYsixteen = document.getElementById("sixteenBYsixteen");
+function handleClick(buttonId){
+    if(buttonId === 'reset'){
+        location.reload();
+    }
+    else if(buttonId ==='a'){  
+        creatGrid4x4();
+        console.log('a');
+    }
+    else if(buttonId ==='b'){
+        creatGrid8x8();
+        console.log('b');
+    }
+    else if(buttonId ==='c'){
+        creatGrid16x16();
+        console.log('c');
+    }
+   
+    
+}
 
-fourBYfour.addEventListener("click", function(){
-
-   creatGrid4x4();
-     
-});
-  
-eightBYeight.addEventListener("click", function(){
-
-    creatGrid8x8();
-      
- });
-
-sixteenBYsixteen.addEventListener("click", function(){
-
-    creatGrid16x16();
-      
-});
 
 
 
